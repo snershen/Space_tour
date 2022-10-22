1cfdd6de8aed448522a0641be8b329ed4701b472
Wu Jane <jane72418@gmail.com>
2022-10-23 01:25:41 +0800

    ver 1

---
+++ app/assets/js/all.js
@@ -0,0 +1,79 @@
+// navbar mobile
+$(".ham-menu").click((e) => {
+  e.preventDefault();
+  $(".nav").toggleClass("d-flex");
+  $(".ham-menu").toggleClass("ham-menu--close");
+});
+
+//navbar active
+window.addEventListener("load", function () {
+  const currentPage = document.querySelector("main");
+  const navLink = document.querySelectorAll(".nav-link");
+  navLink.forEach((i) => {
+    if (
+      currentPage.getAttribute("data-page") === i.getAttribute("data-navLink")
+    ) {
+      i.classList.add("nav-link--active");
+    }
+  });
+});
+
+//Swiper
+var crewSwiper = new Swiper(".crew-swiper", {
+  slidesPerView: 1,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  loop: true,
+  speed: 300,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: {
+    invert: false,
+  },
+});
+
+var technologySwiper = new Swiper(".technology-swiper", {
+  slidesPerView: 1,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  loop: true,
+  speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  mousewheel: {
+    invert: false,
+  },
+});
+
+const planet = ["MOON", "MARS", "EUROPA", "TITAN"];
+var destination = new Swiper(".destination-swiper", {
+  slidesPerView: 1,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  loop: true,
+  // speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + planet[index] + "</span>";
+    },
+  },
+  mousewheel: {
+    invert: false,
+  },
+});
